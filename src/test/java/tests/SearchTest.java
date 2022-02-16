8241833e7267ff838cc423bed9ad0205b1e9f906
mateusfjs <78116134+mateusfjs@users.noreply.github.com>
2022-02-16 15:35:34 -0300

    First commit

---
+++ src/test/java/tests/SearchTest.java
@@ -0,0 +1,51 @@
+package tests;
+
+import io.opentelemetry.exporter.logging.SystemOutLogExporter;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+
+public class SearchTest {
+
+
+    private WebDriver navegador;
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver","c:\\Driver\\chromedriver.exe");
+        navegador = new ChromeDriver();
+        navegador.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void buscarProdutoTest(){
+
+
+        navegador.get("https://www.magazineluiza.com.br/");
+        navegador.findElement(By.id("input-search")).click();
+        navegador.findElement(By.id("input-search")).sendKeys("Notebook");
+        navegador.findElement(By.id("input-search")).sendKeys(Keys.ENTER);
+        String text = navegador.findElement(By.xpath("//*[@id=\"__next\"]/div/main/section[4]/div[3]/div/ul/li[1]/a/div[2]/h2")).getText();
+        System.out.println(text);
+        Assert.assertEquals("Notebook Acer A515-54-59BU Intel Core i5 8GB", text);
+
+    }
+
+
+    @Test
+    public void buscarProdutoFLuxoAlternativoTeste(){
+
+        navegador.get("https://www.magazineluiza.com.br/");
+        navegador.findElement(By.xpath("//*[@id=\"__next\"]/div/main/section[1]/div[2]/header/div/div[3]/nav/ul/li[7]/div[1]/a")).click();
+        navegador.findElement(By.xpath("//*[@id=\"sideNavigation\"]/nav/ul/li[1]/ul/li[1]/a")).click();
+        String text = navegador.findElement(By.xpath("//*[@id=\"showcase\"]/ul[1]/a[1]/div[3]/h3")).getText();
+        System.out.println(text);
+        Assert.assertEquals("Notebook Acer A515-54-59BU Intel Core i5 8GB", text);
+
+    }
+}
